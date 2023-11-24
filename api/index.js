7198f8f3c3ad8d38c560954c53baa8a4ae1e2b5d
abhayrsr <abhayrajsinghrana123@gmail.com>
2023-11-24 23:00:43 +0530

    port setting

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
-const express = require("express");
+const express = require('express');
 const cors = require('cors');
 const app = express()
-
-app.use(cors());
-app.get('/', (req, res) => {
+const port = 5000;
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    optionSuccessStatus:200
+}));
+app.get('/test', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(5000);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
